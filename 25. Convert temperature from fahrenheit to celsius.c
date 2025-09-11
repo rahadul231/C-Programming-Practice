6824235f2accba8b344eac5de83048122fad2ee4
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-09-12 00:20:05 +0600

    Delete 25. Convert temperature from fahrenheit to celsius.c

--- 25. Convert temperature from fahrenheit to celsius.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float fahrenheit;
-    printf("Enter the value of temperature in celsius: ");
-    scanf("%f",&fahrenheit);
-
-    float celsius = ((fahrenheit-32)*5)/9;
-    printf("Temperature in celsius: %f",celsius);
-}
