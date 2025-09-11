9d1e7bc5092105ad639ea7a18cb99b4058bb036d
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-09-12 00:19:04 +0600

    Delete 29. Convert temperature from Kelvin to fahrenheit.c

--- 29. Convert temperature from Kelvin to fahrenheit.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float kelvin;
-    printf("Enter the value of temperature in kelvin: ");
-    scanf("%f",&kelvin);
-
-    float fahrenheit = (((kelvin-273)*9)/5)+32;
-    printf("Temperature in fahrenheit: %f",fahrenheit);
-}
