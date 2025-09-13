271c82573a8ac15f1df31611e94ec1cb6efce1c2
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-09-14 01:24:25 +0600

    Delete 27. Convert temperature from Kelvin to celsius.c

--- 27. Convert temperature from Kelvin to celsius.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float kelvin;
-    printf("Enter the value of temperature in kelvin: ");
-    scanf("%f",&kelvin);
-
-    float celsius = kelvin-273;
-    printf("Temperature in celsius: %f",celsius);
-}
