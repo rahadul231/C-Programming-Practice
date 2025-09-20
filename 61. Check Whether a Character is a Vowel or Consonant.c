68f598d95fc047b6f4b45de5f5204cfc9420fe05
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-09-21 00:35:00 +0600

    Add files via upload

---
+++ 61. Check Whether a Character is a Vowel or Consonant.c
@@ -0,0 +1,34 @@
+//Check Whether a Character is a Vowel or Consonant
+
+#include<stdio.h>
+int main(){
+    char letter;
+    printf("Enter a letter: ");
+    scanf("%c",&letter);
+    
+    if(letter=='a')
+        printf("Vawel");
+    else if(letter=='a')
+        printf("Vawel");
+    else if(letter=='e')
+        printf("Vawel");
+    else if(letter=='i')
+        printf("Vawel");
+    else if(letter=='o')
+        printf("Vawel");
+    else if(letter=='u')
+        printf("Vawel");
+    else if(letter=='A')
+        printf("Vawel");
+    else if(letter=='E')
+        printf("Vawel");
+    else if(letter=='I')
+        printf("Vawel");
+    else if(letter=='O')
+        printf("Vawel");
+    else if(letter=='U')
+        printf("Vawel");
+    else 
+        printf("Consonent");
+    return 0;
+}
