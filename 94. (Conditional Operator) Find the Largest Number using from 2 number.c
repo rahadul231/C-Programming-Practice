d95066fac2f907d5156d00d0f0d545ac635f23fe
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-10-02 00:25:51 +0600

    Add files via upload

---
+++ 94. (Conditional Operator) Find the Largest Number using from 2 number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter 2 numbers: ");
+    scanf("%d %d",&num1, &num2);
+    
+    int large = (num1>num2)? num1 : num2;
+    printf("Large number is: %d",large);
+    return 0;
+}
