b50cedba97d6b8c670c8f939f7204d4b1d0dffba
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-10-25 21:28:21 +0600

    Add files via upload

---
+++ 98. Find the Smallest number (using logical and).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter 2 number: ");
+    scanf("%d %d", &num1, &num2);
+    
+    int large = (num1<num2)? num1 : num2;
+    printf("Large number is: %d", large);
+    
+    return 0;
+}
